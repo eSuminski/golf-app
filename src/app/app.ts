21b46961e223cab2b71527badbca82deacbf6f38
eSuminski <rctolinski@gmail.com>
2026-04-05 00:53:04 +0000

    adding header, toggleable nav bar, and main section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideNav } from './side-nav/side-nav';
+import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SideNav],
+  imports: [RouterOutlet, MatSidenavModule, MatStepperModule, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('golf-app');
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
 }
